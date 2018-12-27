ee83adfee9e04e193a561d55e66475aaaef5a11d
M-Yankov <mihail.yankov.1337@gmail.com>
2018-12-27 22:12:42 +0200

    Web server for testing (Generated from Visual Studio)

---
+++ src/SampleWebForQA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SampleWebForQA
+{
+    public class Program
+    {
+        public const string LocalHostUrl = "http://localhost:5005";
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls(LocalHostUrl)
+                .UseStartup<Startup>();
+    }
+}
